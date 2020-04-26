c9539774c4009aa3e2e059897c463983a9bb5880
Saray <sarayalth@gmail.com>
2020-04-26 03:12:42 +0200

    Add project files.

---
+++ ezHeight/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BeatSaberMarkupLanguage.Settings;
+using BS_Utils.Utilities;
+using IPA;
+using IPALogger = IPA.Logging.Logger;
+namespace ezHeight
+{
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	public class Plugin
+	{
+		internal static Plugin instance { get; private set; }
+		internal static string Name => "ezHeight";
+		internal static IPALogger log { get; set; }
+		[Init]
+		public void Init(IPALogger logger)
+		{
+			instance = this;
+			log = logger;
+		}
+		[OnStart]
+		public void OnApplicationStart()
+		{
+			BSEvents.menuSceneLoadedFresh += BSEvents_menuSceneLoadedFresh;
+		}
+
+		private void BSEvents_menuSceneLoadedFresh()
+		{
+			BSMLSettings.instance.AddSettingsMenu("ezHeight", "ezHeight.UI.Settings.bsml", UI.Settings.instance);
+		}
+
+		[OnExit]
+		public void OnApplicationQuit(){}
+	}
+}
---
+++ ezHeight/UI/Settings.cs
@@ -0,0 +1,23 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using System.Linq;
+using UnityEngine;
+namespace ezHeight.UI
+{
+	class Settings : PersistentSingleton<Settings>
+	{
+		public PlayerDataModel _playerDataModel;
+
+		public void Awake()
+		{
+			_playerDataModel = Resources.FindObjectsOfTypeAll<PlayerDataModel>().FirstOrDefault();
+		}
+
+		[UIValue("player-height")]
+		public float playerHeight {
+			get => _playerDataModel.playerData.playerSpecificSettings.playerHeight;
+			set {
+				_playerDataModel.playerData.playerSpecificSettings.playerHeight = value;
+			}
+		}
+	}
+}
